9579f54dcbe6c677621a4e882ff9f715ab3d2048
Timothy Kachler <kachler@mac.com>
2019-01-17 14:20:37 -0800

    added pooling to database connection

--- server/controllers/cocktailController.js
+++ server/controllers/cocktailController.js
@@ -1,90 +1,67 @@
-const db = require('../models/database');
+const pool = require('../models/database');
 
-function getRecipes(req, res) {
-  // Declaring recipe array to store all recipe objects
+/** The getRecipes method works with the GET /recipes route.
+ *  The method queries the database for recipes and constructs
+ *  an array of recipe objects which is returned as JSON in the response
+ * */
+async function getRecipes (req, res) {
   const recipeArray = [];
 
-  // Query string to get all recipes
-  const queryString1 = 'SELECT * FROM Recipes';
-  const queryString2 = 'SELECT * FROM Instructions';
-  const queryString3 = 'SELECT ri.r_id, i.i_name, ri.amount FROM Recipe_Ingredient as ri INNER JOIN Ingredients as i ON ri.i_id = i.id INNER JOIN Recipes as r ON ri.r_id = r.id WHERE 1=1';
+  // Defining the necessary queries
+  const queryFindAll = 'SELECT * FROM Recipes';
+  const queryFindInstructions = 'SELECT * FROM Instructions';
+  const queryFindIngredients = `SELECT ri.r_id, i.i_name, ri.amount 
+    FROM Recipe_Ingredient as ri 
+    INNER JOIN Ingredients as i ON ri.i_id = i.id
+    INNER JOIN Recipes as r ON ri.r_id = r.id WHERE 1=1`;
 
-  // Initial query to get all data from recipe table
-  db.query(queryString1, (err, results) => {
-    // Handling query errors
-    if (err) { console.log(err); }
+  // Defining variables for the query results
+  let findAllResponse;
+  let findInstructionsResponse;
+  let findIngredientsResponse;
 
-    // Iterating through results array and creating recipe objects
-    for (let i = 0; i < results.length; i += 1) {
-      const recipeObj = {
-        id: results[i].id,
-        name: results[i].r_name,
-        description: results[i].r_description,
-        picture: results[i].r_pic_url,
-        ingredients: [],
-        amounts: [],
-        steps: []
-      };
-      // Push recipe object to recipe array
-      recipeArray.push(recipeObj);
-    }
-    // console.log(recipeArray);
-  });
+  // Executing each database query asynchronously and storing results
+  try {
+    findAllResponse = await pool.query(queryFindAll);
+    findInstructionsResponse = await pool.query(queryFindInstructions);
+    findIngredientsResponse = await pool.query(queryFindIngredients);
+  } catch (error) {
+    console.log(error);
+  }
+  // Contructing the recipe objects and pushing to the recipe array
+  for (let i = 0; i < findAllResponse.length; i += 1) {
+    const recipeObj = {
+      id: findAllResponse[i].id,
+      name: findAllResponse[i].r_name,
+      description: findAllResponse[i].r_description,
+      picture: findAllResponse[i].r_pic_url,
+      ingredients: [],
+      amounts: [],
+      steps: []
+    };
+    recipeArray.push(recipeObj);
+  }
 
-  db.query(queryString2, (err2, results2) => {
-    for (let i = 0; i < results2.length; i += 1) {
-      for (let x = 0; x < recipeArray.length; x += 1) {
-        if (results2[i].r_id === recipeArray[x].id) {
-          recipeArray[x].steps.push(results2[i].steps_direction);
-        }
+  // Adding the instructions to the recipe objects
+  for (let i = 0; i < findInstructionsResponse.length; i += 1) {
+    for (let x = 0; x < recipeArray.length; x += 1) {
+      if (findInstructionsResponse[i].r_id === recipeArray[x].id) {
+        recipeArray[x].steps.push(findInstructionsResponse[i].steps_direction);
       }
     }
-  });
-  db.query(queryString3, (err3, results3) => {
-    for (let i = 0; i < results3.length; i += 1) {
-      for (let x = 0; x < recipeArray.length; x += 1) {
-        if (results3[i].r_id === recipeArray[x].id) {
-          recipeArray[x].ingredients.push(results3[i].i_name);
-          recipeArray[x].amounts.push(results3[i].amount);
-        }
+  }
+
+  // Adding the ingredients to the recipe objects
+  for (let i = 0; i < findIngredientsResponse.length; i += 1) {
+    for (let x = 0; x < recipeArray.length; x += 1) {
+      if (findIngredientsResponse[i].r_id === recipeArray[x].id) {
+        recipeArray[x].ingredients.push(findIngredientsResponse[i].i_name);
+        recipeArray[x].amounts.push(findIngredientsResponse[i].amount);
       }
     }
-    res.send(recipeArray);
-  });
+  }
+  // Returning the recipe array
+  res.send(recipeArray);
 }
-
-// /**
-//  * Controller method for the GET request /ingredients route
-//  * Queries the RDS/MySQL database go all ingredients
-//  *
-//  * @returns {json} - the response will include all the ingredients in the database
-//  */
-// function getIngredients(req, res) {
-//   console.log('Starting query...');
-//   const queryString = 'SELECT * FROM Ingredients';
-//   db.query(queryString, (err, results) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(results);
-//     res.send(results);
-//   });
-// }
-
-// /**
-//  * Controller method for the POST request /ingredients route
-//  * Queries the RDS/MySQL database with the passed ingredients
-//  *
-//  * @param {array} req.body - req.body will contain an array of the passsed ingredients
-//  * @returns {json} - the response will include all the recipes that contain the passed ingredients
-//  */
-// function checkIngredients(req, res) {
-//   console.log('Starting query...');
-//   const { ingredients } = req.body;
-//   const queryValues = [...ingredients];
-//   console.log(queryValues);
-//   const queryString = 'Add nifty query string here...';
-//   res.send(ingredients);
-// }
 
 module.exports = { getRecipes };
--- server/models/database.js
+++ server/models/database.js
@@ -1,19 +1,34 @@
 const mysql = require('mysql');
+const util = require('util');
 
-// Setting up database connection
-const db = mysql.createConnection({
+// Create a pool of database connections
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-// Connecting to database
-db.connect((err) => {
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
   if (err) {
-    console.log(err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.');
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.');
+    }
   }
-  console.log('Connected to database...');
+  if (connection) connection.release();
+  // eslint-disable-next-line no-useless-return
+  return;
 });
 
-module.exports = db;
+// Promisify for Node.js async/await.
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
